ad7210e662f7f6006e193627e419d156598ae72b
OpenWrt01 <57708567+OpenWrt01@users.noreply.github.com>
2025-04-22 09:48:41 +0800

    mediatek: filogic: Add support for OpenFi 6C 4G/5G CPE.
            Specification:
             * Mediatek MT7981BA
             * 256 MB SPI-NAND
             * 1 GB DDR4 RAM
             * MT7976CN DBDC AX Wi-Fi
             * Internal Gbe Phy (1x LAN Gigabit port)
             * 3x LED (system, internet, wifi)
             * 2x buttons (wps, reset)
             * 1x USB 3.0 port
             * 1x TF card slot
             * 1x USB Modem.
             * 2x SIM card slot
            Serial Interface:
             * 3 Pins GND, RX, TX
             * Settings: 115200, 8N1
    
            Flash instruction:
            1. Press the reset button for 5 seconds while powering on.
            2. Set the computer's IP address to 192.168.1.x.
            3. Open a web browser and access 192.168.1.1.
            4. Select firmware upgrade.

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -814,3 +814,39 @@ define Device/Airpi-emmc-manper
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += Airpi-emmc-manper
+
+define Device/openfi_6c
+  DEVICE_VENDOR := OpenFi
+  DEVICE_MODEL := 6C (with 256MB+1GB+M.2 for 5G+U3)
+  DEVICE_DTS := mt7981-openfi-6c
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := openfi,6c
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 239104k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) kmod-hwmon-pwmfan kmod-usb-net-rndis kmod-usb-serial-option f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc \
+	luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
+endef
+TARGET_DEVICES += openfi_6c
+
+define Device/openfi_6c1
+  DEVICE_VENDOR := OpenFi
+  DEVICE_MODEL := 6C1 (with 256MB+1GB+M.2 for 5G+U3)
+  DEVICE_DTS := mt7981-openfi-6c1
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := openfi,6c1
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 239104k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) kmod-hwmon-pwmfan kmod-usb-net-rndis kmod-usb-serial-option f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc \
+	luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
+endef
+TARGET_DEVICES += openfi_6c1
